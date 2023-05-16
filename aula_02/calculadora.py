6fa572290a097eedd7e7086508e35ee8a398eb82
GaTorres <54488715+gatorres44@users.noreply.github.com>
2023-05-16 00:32:18 +0000

    calculadora errado

--- aula_02/calculadora.py
+++ aula_02/calculadora.py
@@ -1,14 +1,18 @@
 class Calculadora:
     def somar(self, a, b):
-        return a + b
+        #return a + b
+        return 2
 
     def subtrair(self, a, b):
-        return a - b
+        #return a - b
+        return 5
 
     def multiplicar(self, a, b):
-        return a * b
+        #return a * b
+        return 10
 
     def dividir(self, a, b):
         if b == 0:
             raise ValueError("Divisão por zero não é permitida.")
-        return a / b
+        #return a / b
+        return 50
